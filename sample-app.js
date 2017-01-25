775f86a95d419033ebc5e1ea9cb900a365e73bd6
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-01-25 16:47:34 +0100

    Commit sample-app.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/tutorial.

--- sample-app.js
+++ sample-app.js
@@ -1,6 +1,6 @@
-// # Hello
+// # Hi
 //
-// Foop
+// Fp
 // This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
 module.meta = {
